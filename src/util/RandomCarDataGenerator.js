582808a1fb9b102b83f35cb5fa352294df7d50fa
sridhar <sridhar@zeliot.in>
2026-04-16 16:12:29 +0530

    card id random generated

--- src/util/RandomCarDataGenerator.js
+++ src/util/RandomCarDataGenerator.js
@@ -8,20 +8,24 @@ const Location = require('../models/Location');
  * Generates a Car with a nested Location object (new in this version).
  */
 class RandomCarDataGenerator {
-  constructor(carId, carNumber) {
-    this.carId     = carId     || '007';
+  constructor(carNumber) {
     this.carNumber = carNumber || 'KA07JB007';
   }
 
   generateRandomCar() {
+    const randomCarId = this._randomCarId();
     return new Car(
-      this.carId,
+      randomCarId,
       this.carNumber,                              // maps to car.setCarName(carNumberString) in Java
       this._randomSpeed(),
       new Location(this._randomLatitude(), this._randomLongitude())  // nested Location
     );
   }
 
+   _randomCarId() {
+    const num = Math.floor(Math.random() * 15) + 1;
+    return num.toString().padStart(2, '0');
+  }
   _randomSpeed()     { return Math.random() * 180; }          // 0 – 180 km/h
   _randomLatitude()  { return -90  + Math.random() * 180; }   // -90  to  90
   _randomLongitude() { return -180 + Math.random() * 360; }   // -180 to 180
